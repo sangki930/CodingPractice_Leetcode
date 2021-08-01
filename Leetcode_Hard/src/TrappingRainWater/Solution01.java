192ee4e0389b6abb249e7f8be70ea69da54d1729
sangkipark <sangki931@gmail.com>
2021-08-01 13:26:28 +0900

    TrappingRainWater commit

---
+++ Leetcode_Hard/src/TrappingRainWater/Solution01.java
@@ -0,0 +1,23 @@
+package TrappingRainWater;
+
+public class Solution01 {
+	public int trap(int[] height) {
+        int result = 0;
+        int start = 0;
+        int end = height.length - 1;
+        while (start < end) {
+            if (height[start] <= height[end]) {
+                int current = height[start];
+                while (height[++start] < current) {
+                    result += current - height[start];
+                }
+            } else {
+                int current = height[end];
+                while(height[--end] < current) {
+                    result += current - height[end];
+                }
+            }
+        }
+        return result;
+    }
+}
