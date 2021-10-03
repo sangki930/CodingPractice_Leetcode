b03494825005e6f9965dc13b347654b07b37584c
sangkipark <sangki931@gmail.com>
2021-10-03 10:23:44 +0900

    coommit

---
+++ Leetcode_Medium/src/JumpGame/Solution.java
@@ -0,0 +1,14 @@
+package JumpGame;
+
+public class Solution {
+
+	 public boolean canJump(int[] nums) {
+	        int lastGoodIndex = nums[nums.length-1];
+	        for (int i = nums.length-1; i >=0; i--){
+	            if (i + nums[i] >= lastGoodIndex) { 
+	                lastGoodIndex = i;
+	            }
+	        }
+	        return (lastGoodIndex == 0);
+	   }
+}
