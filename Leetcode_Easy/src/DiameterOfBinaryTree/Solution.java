b06386919a26de92b064bc0d139c72d31bad7229
sangkipark <sangki931@gmail.com>
2021-10-11 18:47:01 +0900

    commit

--- Leetcode_Easy/src/DiameterOfBinaryTree/Solution.java
+++ Leetcode_Easy/src/DiameterOfBinaryTree/Solution.java
@@ -1,5 +1,33 @@
 package DiameterOfBinaryTree;
 
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
+ 
 public class Solution {
-
+    int answer=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return this.answer;
+    }
+    
+    public int dfs(TreeNode node){
+        if(node==null)
+            return -1;
+        
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        this.answer = Math.max(this.answer,left+right+2);
+        
+        return Math.max(left,right)+1;
+    }
 }
