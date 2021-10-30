e389920ae6d3b2d50e1b5d7f488bd5530917cce6
sangkipark <sangki931@gmail.com>
2021-10-30 23:19:49 +0900

    commit

--- Leetcode_Easy/src/PalindromeNumber/Solution.java
+++ Leetcode_Easy/src/PalindromeNumber/Solution.java
@@ -3,11 +3,9 @@ package PalindromeNumber;
 public class Solution {
 	public boolean isPalindrome(int x) {
         if(x>=0){
-            
             StringBuilder sb = new StringBuilder(x+"");
             if(sb.reverse().toString().equals(x+""))
                 return true;
-            
         }
         return false;
     }
