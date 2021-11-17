14ba205310551c8289d38093a447a73052c1bc9e
sangkipark <sangki931@gmail.com>
2021-11-17 22:41:40 +0900

    commit

--- Leetcode_Medium/src/UniquePaths/Solution.java
+++ Leetcode_Medium/src/UniquePaths/Solution.java
@@ -10,8 +10,7 @@ public class Solution {
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-            
+            } 
         }
         return dp[m-1][n-1];
     }
